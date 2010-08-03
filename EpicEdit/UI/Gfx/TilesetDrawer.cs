664e02b2377bd455ac38e94262d8900666ef7c6a
Thomas Goldstein <goldstein.thomas@gmail.com>
2010-08-03 23:09:35 +0200

    Cache tileset to improve performances (painting is almost twice faster)

--- EpicEdit/UI/Gfx/TilesetDrawer.cs
+++ EpicEdit/UI/Gfx/TilesetDrawer.cs
@@ -30,6 +30,7 @@ namespace EpicEdit.UI.Gfx
 		private Tile[] tileset;
 
 		private Graphics tilesetGfx;
+		private Bitmap tilesetCache;
 		private Pen tilesetPen;
 
 		public TilesetDrawer(Control control)
@@ -39,33 +40,44 @@ namespace EpicEdit.UI.Gfx
 			this.tilesetGfx.PixelOffsetMode = PixelOffsetMode.Half; // Solves a GDI+ bug which crops scaled images
 
 			this.tilesetPen = new Pen(Color.FromArgb(150, 255, 0, 0), 2);
+
+			// The following member is initialized so it can be disposed of
+			// in each function without having to check if it's null beforehand
+			this.tilesetCache = new Bitmap(1, 1, PixelFormat.Format32bppPArgb);
 		}
 
 		public void SetTileset(Tile[] tileset)
 		{
 			this.tileset = tileset;
+
+			this.UpdateCache();
+		}
+
+		private void UpdateCache()
+		{
+			this.tilesetCache.Dispose();
+
+			this.tilesetCache = new Bitmap(64, 256, PixelFormat.Format32bppPArgb);
+			using (Graphics gfx = Graphics.FromImage(this.tilesetCache))
+			{
+				for (int x = 0; x < 8; x++)
+				{
+					for (int y = 0; y < 32; y++)
+					{
+						Tile tile = this.tileset[x + (y * 8)];
+						gfx.DrawImage(tile.Bitmap, x * 8, y * 8);
+					}
+				}
+			}
 		}
 
 		public void DrawTileset(byte selectedTile)
 		{
 			int zoom = 2;
 
-			using (Bitmap tilesetImage = new Bitmap(64, 256, PixelFormat.Format32bppPArgb))
-			{
-				using (Graphics gfx = Graphics.FromImage(tilesetImage))
-				{
-					for (int x = 0; x < 8; x++)
-					{
-						for (int y = 0; y < 32; y++)
-						{
-							Tile tile = this.tileset[x + (y * 8)];
-							gfx.DrawImage(tile.Bitmap, x * 8, y * 8);
-						}
-					}
-				}
-
-				this.tilesetGfx.DrawImage(tilesetImage, 0, 0, tilesetImage.Width * zoom, tilesetImage.Height * zoom);
-			}
+			this.tilesetGfx.DrawImage(this.tilesetCache, 0, 0,
+									  this.tilesetCache.Width * zoom,
+									  this.tilesetCache.Height * zoom);
 
 			int tilePosX = selectedTile % 8;
 			int tilePosY = selectedTile / 8;
@@ -77,6 +89,7 @@ namespace EpicEdit.UI.Gfx
 		public void Dispose()
 		{
 			this.tilesetGfx.Dispose();
+			this.tilesetCache.Dispose();
 			this.tilesetPen.Dispose();
 
 			GC.SuppressFinalize(this);
