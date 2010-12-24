b7407886a0921cb62b8a7ffa35d981cc5e9d0cca
Thomas Goldstein <goldstein.thomas@gmail.com>
2010-12-24 11:44:10 +0100

    Add new "Add" button

--- EpicEdit/Properties/Resources.Designer.cs
+++ EpicEdit/Properties/Resources.Designer.cs
@@ -60,6 +60,13 @@ namespace EpicEdit.Properties {
             }
         }
         
+        internal static System.Drawing.Bitmap AddButton {
+            get {
+                object obj = ResourceManager.GetObject("AddButton", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
         internal static System.Drawing.Bitmap DeleteButton {
             get {
                 object obj = ResourceManager.GetObject("DeleteButton", resourceCulture);
