29625c3d7a9fd9952169c50a8f2a15959e136f48
Thomas Goldstein <goldstein.thomas@gmail.com>
2011-09-24 20:02:10 +0200

    On color picking, get the palette theme, rather than the current track theme (safer, and fixes item icon color picking)

--- EpicEdit/UI/TrackEdition/TrackEditor.cs
+++ EpicEdit/UI/TrackEdition/TrackEditor.cs
@@ -802,7 +802,6 @@ namespace EpicEdit.UI.TrackEdition
 
         private void TileColorSelected(object sender, EventArgs<Palette, int> e)
         {
-            this.paletteForm.Editor.Theme = this.track.Theme;
             this.paletteForm.Editor.SetPalette(e.Value1);
             this.paletteForm.Editor.SetColorIndex(e.Value2);
         }
