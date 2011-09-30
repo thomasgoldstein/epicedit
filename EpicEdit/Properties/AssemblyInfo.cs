daaa33ece15c0d761e62aa5831ab8f01b95d239a
Thomas Goldstein <goldstein.thomas@gmail.com>
2011-09-30 22:17:26 +0200

    Update version to 2.3

--- EpicEdit/Properties/AssemblyInfo.cs
+++ EpicEdit/Properties/AssemblyInfo.cs
@@ -48,4 +48,4 @@ using System.Runtime.InteropServices;
 // You can specify all values by your own or you can build default build and revision
 // numbers with the '*' character (the default):
 
-[assembly: AssemblyVersion("2.2")]
+[assembly: AssemblyVersion("2.3")]
