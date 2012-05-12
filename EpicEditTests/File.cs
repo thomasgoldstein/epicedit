a1cb6ba380f7099563f32e50e930babe06e296f5
Thomas Goldstein <goldstein.thomas@gmail.com>
2012-05-12 21:34:46 +0200

    Simplify and optimize the File helper class

--- EpicEditTests/File.cs
+++ EpicEditTests/File.cs
@@ -31,28 +31,19 @@ namespace EpicEditTests
 
         public static byte[] ReadFile(string filePath)
         {
-            using (FileStream fs = new FileStream(File.RelativePath + filePath, FileMode.Open, FileAccess.Read))
-            using (BinaryReader br = new BinaryReader(fs))
-            {
-                int size = (int)br.BaseStream.Length;
-                byte[] buffer = br.ReadBytes(size);
-                return buffer;
-            }
+            return System.IO.File.ReadAllBytes(File.RelativePath + filePath);
         }
 
         public static byte[] ReadBlock(byte[] buffer, int offset, int length)
         {
             byte[] bytes = new byte[length];
-            Array.Copy(buffer, offset, bytes, 0, length);
+            Buffer.BlockCopy(buffer, offset, bytes, 0, length);
             return bytes;
         }
 
         public static void WriteFile(byte[] array, string fileName)
         {
-            using (BinaryWriter bw = new BinaryWriter(new FileStream(fileName, FileMode.Create, FileAccess.Write)))
-            {
-                bw.Write(array);
-            }
+            System.IO.File.WriteAllBytes(fileName, array);
         }
     }
 }
