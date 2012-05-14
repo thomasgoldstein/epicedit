c180982951d83788e4da7bf52d74041b8fc1c05c
Thomas Goldstein <goldstein.thomas@gmail.com>
2012-05-14 03:30:19 +0200

    Dispose of repaintTimer

--- EpicEdit/UI/ThemeEdition/BackgroundPreviewer.Designer.cs
+++ EpicEdit/UI/ThemeEdition/BackgroundPreviewer.Designer.cs
@@ -33,8 +33,9 @@ namespace EpicEdit.UI.ThemeEdition
                 {
                     components.Dispose();
                 }
-                
+
                 this.drawer.Dispose();
+                this.repaintTimer.Dispose();
             }
             base.Dispose(disposing);
         }
