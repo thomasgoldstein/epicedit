d842bbdef0d46a1f5c9e0d53fe59abaf3b7ad389
Thomas Goldstein <goldstein.thomas@gmail.com>
2012-05-22 00:09:27 +0200

    Add hack to fix object zone loading from ROMs resaved with EE 2.2+

--- EpicEdit/Rom/Offsets.cs
+++ EpicEdit/Rom/Offsets.cs
@@ -417,7 +417,14 @@ namespace EpicEdit.Rom
             this[Offset.TrackObjectHack7] = 0x4DCBD;
             this[Offset.TrackObjectHack8] = 0x4DCC2;
             this[Offset.TrackObjectProperties] = 0x80062;
-            this[Offset.TrackObjectZonesRelocated] = 0x80219;
+            this[Offset.TrackObjectZonesRelocated] = Utilities.BytesToOffset(Utilities.ReadBlock(romBuffer, this[Offset.TrackObjectHack5] + 1, 3)) + 0x6E;
+
+            if (romBuffer[this[Offset.TrackObjectHack3] + 1] == 0x28)
+            {
+                // HACK: The ROM has been resaved with Epic Edit 2.2, 2.3, 2.4 or 2.4.1.
+                this[Offset.TrackObjectZonesRelocated] -= 0x03;
+            }
+
             this[Offset.TrackObjectPaletteIndexes] = Utilities.BytesToOffset(Utilities.ReadBlock(romBuffer, this[Offset.TrackObjectHack3] + 1, 3)) + 0x13;
             this[Offset.TrackObjectFlashing] = this[Offset.TrackObjectPaletteIndexes] + Track.Count * 4;
             this[Offset.CommonTilesetGraphicsLowerBytes] = this[Offset.CommonTilesetGraphicsUpperByte] + 3;
