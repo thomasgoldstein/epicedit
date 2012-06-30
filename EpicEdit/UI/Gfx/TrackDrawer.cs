dfbf97c867f97f04ab9fd6113b041bf7ccce5c9f
Thomas Goldstein <goldstein.thomas@gmail.com>
2012-06-30 10:41:48 +0200

    Remove unnecessary fullRepaintNeeded initialization (as the LoadTrack method will be called before rendering anything and will do it)

--- EpicEdit/UI/Gfx/TrackDrawer.cs
+++ EpicEdit/UI/Gfx/TrackDrawer.cs
@@ -159,7 +159,6 @@ namespace EpicEdit.UI.Gfx
         public TrackDrawer(Control control, float zoom)
         {
             this.control = control;
-            this.fullRepaintNeeded = true;
 
             #region Pens and Brushes initialization
 
