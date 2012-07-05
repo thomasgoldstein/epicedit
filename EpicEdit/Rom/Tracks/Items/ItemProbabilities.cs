039c961802efd38d03493345340d0c9a33bb2be1
Thomas Goldstein <goldstein.thomas@gmail.com>
2012-07-05 20:26:13 +0200

    Remove unused usings + reformatting

--- EpicEdit/Rom/Tracks/Items/ItemProbabilities.cs
+++ EpicEdit/Rom/Tracks/Items/ItemProbabilities.cs
@@ -13,9 +13,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 #endregion
 
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Text;
 
 namespace EpicEdit.Rom.Tracks.Items
 {
