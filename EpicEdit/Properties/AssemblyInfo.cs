07699a6401b3bc187aec0a25c7b29ba3b2041a37
Thomas Goldstein <goldstein.thomas@gmail.com>
2012-12-15 10:57:20 +0100

    Bump version number

--- EpicEdit/Properties/AssemblyInfo.cs
+++ EpicEdit/Properties/AssemblyInfo.cs
@@ -48,4 +48,4 @@ using System.Runtime.InteropServices;
 // You can specify all values by your own or you can build default build and revision
 // numbers with the '*' character (the default):
 
-[assembly: AssemblyVersion("2.6")]
+[assembly: AssemblyVersion("2.6.1")]
