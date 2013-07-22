70ca32cb7546fcd7a0df5d51508d8796093e81b2
Thomas Goldstein <goldstein.thomas@gmail.com>
2013-07-22 18:39:22 +0200

    Remove AutoScale property settings (can cause size issues depending on the configured font, on Windows and with Mono)

--- EpicEdit/UI/Tools/CodecControl.Designer.cs
+++ EpicEdit/UI/Tools/CodecControl.Designer.cs
@@ -108,8 +108,6 @@ namespace EpicEdit.UI.Tools
             // 
             // CodecControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.browseButton);
             this.Controls.Add(this.offsetLabel);
             this.Controls.Add(this.offsetNumericUpDown);
