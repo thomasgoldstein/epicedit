8c83d5bb4552007e085d41d24ee02a101ab06b87
Thomas Goldstein <goldstein.thomas@gmail.com>
2013-07-27 07:32:05 +0200

    Use new Utility namespace

--- EpicEditTests/Rom/UtilitiesTest.cs
+++ EpicEditTests/Rom/UtilitiesTest.cs
@@ -14,6 +14,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 
 using System;
 using EpicEdit.Rom;
+using EpicEdit.Rom.Utility;
 using NUnit.Framework;
 
 namespace EpicEditTests.Rom
