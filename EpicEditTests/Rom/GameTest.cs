bb565fd96ed7a0ed0fbd7c5e63038a2fc4d64b53
Thomas Goldstein <goldstein.thomas@gmail.com>
2013-08-05 22:38:40 +0200

    Rename private field for consistency

--- EpicEditTests/Rom/GameTest.cs
+++ EpicEditTests/Rom/GameTest.cs
@@ -22,23 +22,23 @@ namespace EpicEditTests.Rom
     [TestFixture]
     internal class GameTest
     {
-        private Game rom;
+        private Game game;
 
         public GameTest()
         {
-            this.rom = new Game(File.RelativePath + "smk.smc");
+            this.game = new Game(File.RelativePath + "smk.smc");
         }
 
         [Test]
         public void TestTrackGroupNumber()
         {
-            Assert.AreEqual(5, this.rom.TrackGroups.Count);
+            Assert.AreEqual(5, this.game.TrackGroups.Count);
         }
 
         [Test]
         public void TestTrackSizes()
         {
-            foreach (TrackGroup trackGroup in this.rom.TrackGroups)
+            foreach (TrackGroup trackGroup in this.game.TrackGroups)
             {
                 foreach (Track track in trackGroup)
                 {
@@ -51,73 +51,73 @@ namespace EpicEditTests.Rom
         [Test]
         public void TestTrackGroupName1()
         {
-            Assert.AreEqual("Mushroom Cup", this.rom.TrackGroups[0].Name);
+            Assert.AreEqual("Mushroom Cup", this.game.TrackGroups[0].Name);
         }
 
         [Test]
         public void TestTrackGroupName2()
         {
-            Assert.AreEqual("Flower Cup", this.rom.TrackGroups[1].Name);
+            Assert.AreEqual("Flower Cup", this.game.TrackGroups[1].Name);
         }
 
         [Test]
         public void TestTrackGroupName3()
         {
-            Assert.AreEqual("Star Cup", this.rom.TrackGroups[2].Name);
+            Assert.AreEqual("Star Cup", this.game.TrackGroups[2].Name);
         }
 
         [Test]
         public void TestTrackGroupName4()
         {
-            Assert.AreEqual("Special Cup", this.rom.TrackGroups[3].Name);
+            Assert.AreEqual("Special Cup", this.game.TrackGroups[3].Name);
         }
 
         [Test]
         public void TestTrackGroupName5()
         {
-            Assert.AreEqual("Battle Course ", this.rom.TrackGroups[4].Name);
+            Assert.AreEqual("Battle Course ", this.game.TrackGroups[4].Name);
         }
 
         [Test]
         public void TestTrackName1()
         {
-            Assert.AreEqual("Mario Circuit 1", this.rom.TrackGroups[0][0].Name);
+            Assert.AreEqual("Mario Circuit 1", this.game.TrackGroups[0][0].Name);
         }
 
         [Test]
         public void TestTrackName2()
         {
-            Assert.AreEqual("Donut Plains 1", this.rom.TrackGroups[0][1].Name);
+            Assert.AreEqual("Donut Plains 1", this.game.TrackGroups[0][1].Name);
         }
 
         [Test]
         public void TestTrackName3()
         {
-            Assert.AreEqual("Ghost Valley 1", this.rom.TrackGroups[0][2].Name);
+            Assert.AreEqual("Ghost Valley 1", this.game.TrackGroups[0][2].Name);
         }
 
         [Test]
         public void TestTrackName20()
         {
-            Assert.AreEqual("Rainbow Road ", this.rom.TrackGroups[3][4].Name);
+            Assert.AreEqual("Rainbow Road ", this.game.TrackGroups[3][4].Name);
         }
 
         [Test]
         public void TestTrackTheme1()
         {
-            Assert.AreEqual("Mario Circuit ", this.rom.TrackGroups[0][0].Theme.Name);
+            Assert.AreEqual("Mario Circuit ", this.game.TrackGroups[0][0].Theme.Name);
         }
 
         [Test]
         public void TestTrackTheme2()
         {
-            Assert.AreEqual("Donut Plains ", this.rom.TrackGroups[0][1].Theme.Name);
+            Assert.AreEqual("Donut Plains ", this.game.TrackGroups[0][1].Theme.Name);
         }
 
         [Test]
         public void TestTrackTheme3()
         {
-            Assert.AreEqual("Ghost Valley ", this.rom.TrackGroups[0][2].Theme.Name);
+            Assert.AreEqual("Ghost Valley ", this.game.TrackGroups[0][2].Theme.Name);
         }
     }
 }
