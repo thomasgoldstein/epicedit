0d5d1fbf245db0ea10c5f4972655475542a067a1
Thomas Goldstein <stifu@free.fr>
2015-05-24 10:38:02 +0200

    Updated by SharpDevelop

--- EpicEdit/Properties/AssemblyInfo.cs
+++ EpicEdit/Properties/AssemblyInfo.cs
@@ -11,7 +11,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 #endregion
-
 using System;
 using System.Reflection;
 using System.Resources;
@@ -23,29 +22,24 @@ using System.Runtime.InteropServices;
 //
 // change them to the information which is associated with the assembly
 // you compile.
-
-[assembly: CLSCompliant(true)]
-[assembly: NeutralResourcesLanguage("")]
-[assembly: InternalsVisibleTo("EpicEdit.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d73fe7928261ec039301b4f33ca5228d82bb1f292ebec5050008ba237a40fc53b7608ede38e106cfe75ccd4a74d00ed3605f908490ac38cf8bbc274de29df628d104b1a1097ab21ddb7bdafd098aa7db342cee4d54a62b08a48154137f233e5ba980aa1a37a57c9787e7c3334705aadec05a7f8cfcd8be121fdb7edf9082b9c0")]
-
-[assembly: AssemblyTitle("Epic Edit")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Epic Edit")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
+[assembly: CLSCompliant (true)]
+[assembly: NeutralResourcesLanguage ("")]
+[assembly: InternalsVisibleTo ("EpicEdit.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d73fe7928261ec039301b4f33ca5228d82bb1f292ebec5050008ba237a40fc53b7608ede38e106cfe75ccd4a74d00ed3605f908490ac38cf8bbc274de29df628d104b1a1097ab21ddb7bdafd098aa7db342cee4d54a62b08a48154137f233e5ba980aa1a37a57c9787e7c3334705aadec05a7f8cfcd8be121fdb7edf9082b9c0")]
+[assembly: AssemblyTitle ("Epic Edit")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("Epic Edit")]
+[assembly: AssemblyCopyright ("")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
 // This sets the default COM visibility of types in the assembly to invisible.
 // If you need to expose a type to COM, use [ComVisible(true)] on that type.
-[assembly: ComVisible(false)]
-
+[assembly: ComVisible (false)]
 // The assembly version has following format :
 //
 // Major.Minor.Build.Revision
 //
 // You can specify all values by your own or you can build default build and revision
 // numbers with the '*' character (the default):
-
-[assembly: AssemblyVersion("2.7.2")]
+[assembly: AssemblyVersion ("2.7.2")]
