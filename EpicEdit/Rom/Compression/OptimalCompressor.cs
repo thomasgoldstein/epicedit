5476d2533f99029fc6247e938da7c33ba315196e
Thomas Goldstein <stifu@free.fr>
2015-06-09 22:58:53 +0200

    Move Range struct under the Utility namespace

--- EpicEdit/Rom/Compression/OptimalCompressor.cs
+++ EpicEdit/Rom/Compression/OptimalCompressor.cs
@@ -14,6 +14,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 
 using System;
 using System.Collections.Generic;
+using EpicEdit.Rom.Utility;
 
 namespace EpicEdit.Rom.Compression
 {
