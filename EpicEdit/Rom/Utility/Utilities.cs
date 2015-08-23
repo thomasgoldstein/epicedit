1e61ef8cbc9ba91ff8d239dd1703a305b1491fb7
Thomas Goldstein <stifu@free.fr>
2015-08-24 00:24:28 +0200

    Remove unused usings

--- EpicEdit/Rom/Utility/Utilities.cs
+++ EpicEdit/Rom/Utility/Utilities.cs
@@ -14,8 +14,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 
 using System;
 using System.Drawing;
-using System.Globalization;
-using System.Text;
 
 namespace EpicEdit.Rom.Utility
 {
