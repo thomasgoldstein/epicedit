7e6cd15f883fc90f89e64452f46c3aa289ea9770
Thomas Goldstein <stifu@free.fr>
2017-06-03 18:06:36 +0200

    Update year in About message box

--- EpicEdit/UI/MenuBar.cs
+++ EpicEdit/UI/MenuBar.cs
@@ -273,7 +273,7 @@ namespace EpicEdit.UI
                 version.Major + "." + version.Minor + "." + version.Build;
 
             MessageBox.Show(Application.ProductName + " - Super Mario Kart track editor" + Environment.NewLine +
-                            "Version: " + versionText + " (2007-2015)" + Environment.NewLine +
+                            "Version: " + versionText + " (2007-2017)" + Environment.NewLine +
                             "Site: http://epicedit.stifu.fr/" + Environment.NewLine + Environment.NewLine +
                             "By Stifu" + Environment.NewLine +
                             "Assistant coders: Midwife and teknix1" + Environment.NewLine + Environment.NewLine +
