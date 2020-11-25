35d8ec3da871fe5eba0d37e66132e1490bdfbf8f
Thomas Goldstein <stifu@free.fr>
2020-11-25 13:12:33 +0100

    Migrate csproj files to the new format

--- EpicEdit.Tests/Properties/AssemblyInfo.cs
+++ EpicEdit.Tests/Properties/AssemblyInfo.cs
@@ -27,22 +27,6 @@ using System.Runtime.InteropServices;
 [assembly: Parallelizable(ParallelScope.All)]
 
 [assembly: CLSCompliant(true)]
-[assembly: NeutralResourcesLanguage("")]
-[assembly: AssemblyTitle("Epic Edit Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Epic Edit Tests")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 // This sets the default COM visibility of types in the assembly to invisible.
 // If you need to expose a type to COM, use [ComVisible(true)] on that type.
 [assembly: ComVisible(false)]
-// The assembly version has following format :
-//
-// Major.Minor.Build.Revision
-//
-// You can specify all values by your own or you can build default build and revision
-// numbers with the '*' character (the default):
-[assembly: AssemblyVersion("2.8.2")]
--- EpicEdit/Properties/AssemblyInfo.cs
+++ EpicEdit/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-#region GPL statement
+﻿#region GPL statement
 /*Epic Edit is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, version 3 of the License.
@@ -24,23 +24,7 @@ using System.Runtime.InteropServices;
 // change them to the information which is associated with the assembly
 // you compile.
 [assembly: CLSCompliant (true)]
-[assembly: NeutralResourcesLanguage ("")]
 [assembly: InternalsVisibleTo ("EpicEdit.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d73fe7928261ec039301b4f33ca5228d82bb1f292ebec5050008ba237a40fc53b7608ede38e106cfe75ccd4a74d00ed3605f908490ac38cf8bbc274de29df628d104b1a1097ab21ddb7bdafd098aa7db342cee4d54a62b08a48154137f233e5ba980aa1a37a57c9787e7c3334705aadec05a7f8cfcd8be121fdb7edf9082b9c0")]
-[assembly: AssemblyTitle ("Epic Edit")]
-[assembly: AssemblyDescription ("")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("Epic Edit")]
-[assembly: AssemblyCopyright ("")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
 // This sets the default COM visibility of types in the assembly to invisible.
 // If you need to expose a type to COM, use [ComVisible(true)] on that type.
 [assembly: ComVisible (false)]
-// The assembly version has following format :
-//
-// Major.Minor.Build.Revision
-//
-// You can specify all values by your own or you can build default build and revision
-// numbers with the '*' character (the default):
-[assembly: AssemblyVersion ("2.8.7")]
