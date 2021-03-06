359f8914fa487a5ca20c299f331883682529f413
ChristophSilge <nub1604@web.de>
2021-03-06 11:09:17 +0100

    Some Shortcuts
    - Added shortcut CTRL+D for duplicating a selected ai-element
    - Added shortcut CTRL+M for changing the shape from selected ai-element
    - Added shortcut B for  Bucketdrawing in TileMode
    - Added shortcut G for  Penrawing in TileMode
    - Added shortcut 1-8 for switching the Theme from selected Tile in TileMode

---
+++ EpicEdit/UI/TrackEdition/RoadTilesetControl.Custom.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EpicEdit.UI.TrackEdition
+{
+    internal partial class RoadTilesetControl
+    {
+        public void SetTheme(int number)
+        {
+            var ud = this.tilePaletteNumericUpDown;
+            if (number >= ud.Minimum && number <= ud.Maximum)
+            {
+                this.tilePaletteNumericUpDown.Value = number;
+            }
+        }
+        public void SelectFloodFillTool()
+        {
+            this.bucketButton.PerformClick();
+        }
+        public void SelectPenTool()
+        {
+            this.pencilButton.PerformClick();
+        }
+    }
+}
