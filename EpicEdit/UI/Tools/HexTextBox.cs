30f2e8970a1cf4fc47db1f67c5954499594fee4f
Thomas Goldstein <stifu@free.fr>
2021-03-07 22:49:18 +0100

    Introduce HexTextBox to replace the tbHexString TextBox

---
+++ EpicEdit/UI/Tools/HexTextBox.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace EpicEdit.UI.Tools
+{
+    internal class HexTextBox : NumericUpDown
+    {
+        public int HexValuePadding { get; set; }
+
+        public HexTextBox()
+        {
+            this.Hexadecimal = true;
+
+            // Remove the up/down arrows
+            this.Controls.RemoveAt(0);
+        }
+
+        protected override void OnTextBoxResize(object source, EventArgs e)
+        {
+            // Adapt width to make up for the removed up/down arrows
+            this.Controls[0].Width = this.Width - (SystemInformation.Border3DSize.Width * 2);
+        }
+
+        protected override void UpdateEditText()
+        {
+            // Pad the hex value as configured
+            this.Text = ((int)this.Value).ToString("X" + this.HexValuePadding, CultureInfo.InvariantCulture);
+        }
+    }
+}
